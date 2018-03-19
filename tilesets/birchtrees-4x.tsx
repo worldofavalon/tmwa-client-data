69d8ee02a90a70efaaf7d41574d496ae6cc8fb11
deejay <deejay@deejay.eagle.vn>
2018-03-19 13:15:31 -0500

    Added test island
    - Also added updated tilesets
    - Including some new trees

---
+++ tilesets/birchtrees-4x.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="birchtrees-4x" tilewidth="32" tileheight="32">
+ <image source="../graphics/tiles/birchtrees-4x.png" width="608" height="128"/>
+</tileset>
---
+++ tilesets/leaflesstree-x6.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="leaflesstree-x6" tilewidth="32" tileheight="32">
+ <image source="../graphics/tiles/leaflesstree-x6.png" width="1024" height="192"/>
+</tileset>
